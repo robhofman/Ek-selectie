3d6564283335b206d32d987baa80d76a480a425d
Thibault V <thibault.vanluchene@student.howest.be>
2016-02-23 16:40:53 +0100

    log message

--- backend/statistics.php
+++ backend/statistics.php
@@ -17,19 +17,21 @@
 // }
 
 include 'dbconnect.php';
+$sql = "SELECT id, totaal FROM selectiemaker_inzendingen WHERE id=1";
+if ($db->query($sql) === TRUE) { echo "Query gelukt"; } else { echo "Query mislukt" . $db->error; }
 
-$sql = 'SELECT id, totaal FROM selectiemaker_inzendingen WHERE id=1';
-$result = $db->query($sql);
-
-
-
-$json1 = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//$sql = 'SELECT id, totaal FROM selectiemaker_inzendingen WHERE id=1';
+//$db->query($sql);
 
 
-$sql2 = 'SELECT id, naam, aantal FROM selectiemaker_spelers';
-$result2 = $db->query($sql2);
 
-array_push($json1,mysqli_fetch_all($result2, MYSQLI_ASSOC));
-echo "Hier kom ik";
-echo json_encode($json1);
+//$json1 = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//
+//
+//$sql2 = 'SELECT id, naam, aantal FROM selectiemaker_spelers';
+//$result2 = $db->query($sql2);
+//
+//array_push($json1,mysqli_fetch_all($result2, MYSQLI_ASSOC));
+//echo "Hier kom ik";
+//echo json_encode($json1);
 ?>
