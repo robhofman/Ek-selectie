db4a8440b2fabee2788394921e1d5b9659d96add
Thibault V <thibault.vanluchene@student.howest.be>
2016-02-23 14:04:23 +0100

    aanpassen pad naar statistics.php

--- backend/postSelection.js
+++ backend/postSelection.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 		alert("Uw selectie is succesvol verzonden");
 
 		$.ajax({                                      
-      	url: 'statistics.php',           	//the script to call to get data          
+      	url: 'backend/statistics.php',           	//the script to call to get data
       	data: '',                        	//you can insert url argumnets here to pass to api.php for example "id=5&parent=6"
       	dataType: 'json',                	//data format      
       	success: function(data)          	//on recieve of reply
