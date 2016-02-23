b651ceb662592b7c59f6f4c15e964b165b825627
Thibault V <thibault.vanluchene@student.howest.be>
2016-02-23 16:16:51 +0100

    log message

--- backend/statistics.php
+++ backend/statistics.php
@@ -21,6 +21,8 @@ include 'dbconnect.php';
 $sql = 'SELECT id, totaal FROM selectiemaker_inzendingen WHERE id=1';
 $result = $db->query($sql);
 
+
+
 $json1 = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 
@@ -28,6 +30,6 @@ $sql2 = 'SELECT id, naam, aantal FROM selectiemaker_spelers';
 $result2 = $db->query($sql2);
 
 array_push($json1,mysqli_fetch_all($result2, MYSQLI_ASSOC));
-
+echo "Hier kom ik";
 echo json_encode($json1);
 ?>
