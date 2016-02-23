c53f29f649a8ad9db2b5cc7712c5e6c0e785898a
Thibault V <thibault.vanluchene@student.howest.be>
2016-02-23 15:00:05 +0100

    postSelection.php

--- backend/postSelection.php
+++ backend/postSelection.php
@@ -4,34 +4,36 @@ include 'dbconnect.php';
 $postArray = [];
 $postArray = $_POST;
 
-foreach ($postArray as $value) {
-    switch($value) {
-        case 'Toby Alderweireld':
-            $stmt = $db->prepare("UPDATE selectiemaker_spelers SET aantal=aantal+1 WHERE naam='Toby Alderweireld';");
-            $stmt->execute();
-            $stmt->close();
-            break;
-        case 'Divock Origi':
-            $stmt = $db->prepare("UPDATE selectiemaker_spelers SET aantal=aantal+1 WHERE naam='Divock Origi';");
-            $stmt->execute();
-            $stmt->close();
-            break;
-        case 'Laurent Depoitre':
-            $stmt = $db->prepare("UPDATE selectiemaker_spelers SET aantal=aantal+1 WHERE naam='Laurent Depoitre';");
-            $stmt->execute();
-            $stmt->close();
-            break;       
-        default:
-            echo "User ".$value." not yet in database - Or the switch statement still has to made.";
-            break;
-    }
-}
 
-$stmt2 = $db->prepare("UPDATE selectiemaker_inzendingen SET totaal=totaal+1 WHERE id=1;");
-$stmt2->execute();
-$stmt2->close();
+var_dump($postArray);
+//foreach ($postArray as $value) {
+//    switch($value) {
+//        case 'Toby Alderweireld':
+//            $stmt = $db->prepare("UPDATE selectiemaker_spelers SET aantal=aantal+1 WHERE naam='Toby Alderweireld';");
+//            $stmt->execute();
+//            $stmt->close();
+//            break;
+//        case 'Divock Origi':
+//            $stmt = $db->prepare("UPDATE selectiemaker_spelers SET aantal=aantal+1 WHERE naam='Divock Origi';");
+//            $stmt->execute();
+//            $stmt->close();
+//            break;
+//        case 'Laurent Depoitre':
+//            $stmt = $db->prepare("UPDATE selectiemaker_spelers SET aantal=aantal+1 WHERE naam='Laurent Depoitre';");
+//            $stmt->execute();
+//            $stmt->close();
+//            break;
+//        default:
+//            echo "User ".$value." not yet in database - Or the switch statement still has to made.";
+//            break;
+//    }
+//}
+
+//$stmt2 = $db->prepare("UPDATE selectiemaker_inzendingen SET totaal=totaal+1 WHERE id=1;");
+//$stmt2->execute();
+//$stmt2->close();
 
 
-$db->close();
+//$db->close();
 
 ?>
