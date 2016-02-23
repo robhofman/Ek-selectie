27cfa64a020eb9e8ece6fb68532f9e4d0c1c9805
Thibault V <thibault.vanluchene@student.howest.be>
2016-02-23 14:26:12 +0100

    ajax call aanpassen

--- backend/postSelection.js
+++ backend/postSelection.js
@@ -6,35 +6,37 @@ $(document).ready(function(){
 			$('#checkboxes input:checked').each(function() {
     		selected.push($(this).attr('value'));
     		console.log(selected);
+
+
 		
 		});
 
-		$.post('backend/postSelection.php',
-		{	
-			speler1: selected[0],
-			speler2: selected[1],
-			speler3: selected[2],
-			speler4: selected[3],
-			speler5: selected[4],
-			speler6: selected[5],
-			speler7: selected[6],
-			speler8: selected[7],
-			speler9: selected[8],
-			speler10: selected[9],
-			speler11: selected[10],
-			speler12: selected[11],
-			speler13: selected[12],
-			speler14: selected[13],
-			speler15: selected[14],
-			speler16: selected[15],
-			speler17: selected[16],
-			speler18: selected[17],
-			speler19: selected[18],
-			speler20: selected[19],
-			speler21: selected[20],
-			speler22: selected[21],
-			speler23: selected[22]
-		});
+		$.post('backend/postSelection.php',selected);
+		//{
+			//speler1: selected[0],
+			//speler2: selected[1],
+			//speler3: selected[2],
+			//speler4: selected[3],
+			//speler5: selected[4],
+			//speler6: selected[5],
+			//speler7: selected[6],
+			//speler8: selected[7],
+			//speler9: selected[8],
+			//speler10: selected[9],
+			//speler11: selected[10],
+			//speler12: selected[11],
+			//speler13: selected[12],
+			//speler14: selected[13],
+			//speler15: selected[14],
+			//speler16: selected[15],
+			//speler17: selected[16],
+			//speler18: selected[17],
+			//speler19: selected[18],
+			//speler20: selected[19],
+			//speler21: selected[20],
+			//speler22: selected[21],
+			//speler23: selected[22]
+		//});
 
 		
 		$('#btnBewaar').hide();
