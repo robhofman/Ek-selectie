41d5891583f33eb125d62cc39fbb2cb801072e6b
Thibault V <thibault.vanluchene@student.howest.be>
2016-02-23 23:49:12 +0100

    statistieken ophalen

--- backend/statistics.php
+++ backend/statistics.php
@@ -6,18 +6,27 @@ if (mysqli_connect_errno()) {
     die ('Could not open a mysql connection: '.mysqli_connect_error().'('.mysqli_connect_errno().')');
 }
 
-$sql = 'SELECT * FROM selectiemaker_inzendingen';
-$result = $db->query($sql);
+//$sql = 'SELECT * FROM selectiemaker_inzendingen';
+//$result = $db->query($sql);
+//
+//$json1 = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//
+//
+//$sql2 = 'SELECT id, naam, aantal FROM selectiemaker_spelers';
+//$result2 = $db->query($sql2);
+//
+//array_push($json1,mysqli_fetch_all($result2, MYSQLI_ASSOC));
+//
+//
+//echo json_encode($json1);
 
-$json1 = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$query = mysql_query('SELECT id, naam, aantal FROM selectiemaker_spelers');
 
+while($row = mysql_fetch_assoc($query)){
+    echo $row['id'].'<br>';
+    echo $row['naam'].'<br>';
+    echo $row['aantal'].'<br>';
+}
 
-$sql2 = 'SELECT id, naam, aantal FROM selectiemaker_spelers';
-$result2 = $db->query($sql2);
-
-array_push($json1,mysqli_fetch_all($result2, MYSQLI_ASSOC));
-
-
-echo json_encode($json1);
 
 ?>
