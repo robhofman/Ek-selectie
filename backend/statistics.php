0232d382de12d75e7694f484fcb1009f5ad198c7
Thibault V <thibault.vanluchene@student.howest.be>
2016-02-23 23:30:28 +0100

    statistieken ophalen

--- backend/statistics.php
+++ backend/statistics.php
@@ -1,74 +1,23 @@
 <?php
+
 $mysqli = new mysqli("10.81.0.4", "sql_1004", "wYfVRdGr", "goudenduivels_sporza_be");
 
-/* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
+if (mysqli_connect_errno()) {
+    die ('Could not open a mysql connection: '.mysqli_connect_error().'('.mysqli_connect_errno().')');
 }
 
+$sql = 'SELECT * FROM selectiemaker_inzendingen';
+$result = $db->query($sql);
 
-/* Select queries return a resultset */
-if ($result = $mysqli->query("SELECT * FROM selectiemaker_inzendingen")) {
-    printf("Select returned %d rows.\n", $result->num_rows);
-
-    $rowInzendingen = mysqli_fetch_array($result, MYSQLI_NUM);
-    printf ("%s (%s)\n", $rowInzendingen[0], $rowInzendingen[1]);
+$json1 = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 
-    /* free result set */
-    $result->close();
-}
+$sql2 = 'SELECT id, naam, aantal FROM selectiemaker_spelers';
+$result2 = $db->query($sql2);
 
-if ($result = $mysqli->query("SELECT * FROM selectiemaker_spelers")) {
-    printf("Select returned %d rows.\n", $result->num_rows);
-
-    $rowSpelers = mysqli_fetch_all($result, MYSQLI_BOTH);
-    printf ("%s %s %s\n", $rowSpelers[0], $rowSpelers[1], $rowSpelers[2]);
+array_push($json1,mysqli_fetch_all($result2, MYSQLI_ASSOC));
 
 
+echo json_encode($json1);
 
-    /* free result set */
-    $result->close();
-}
-
-array_push($rowInzendingen, $rowSpelers);
-
-echo json_encode($rowInzendingen);
-
-
-$mysqli->close();
-
-//// include 'dbconnect.php';
-//// $q1 = 'SELECT id, naam, aantal FROM ek_spelers';
-//// $result1 = $db->query($q1);
-//
-//// $q2 = 'SELECT totaal FROM ek_inzendingen';
-//// $result2 = $db->query($q2);
-//
-//// $encode = array();
-//
-//// while($row = mysqli_fetch_assoc($result1)) {
-////    $encode[] = $row;
-//// }
-//
-//// foreach($encode as $key => $value){
-//// 	echo'<p>'.$value.'</p><br>';
-//// }
-//
-//include 'dbconnect.php';
-//
-//
-//$sql = 'SELECT * FROM selectiemaker_inzendingen';
-//$result = $db->query($sql);
-//$json1 = mysqli_fetch_all($result, MYSQLI_ASSOC);
-//
-//
-////$sql2 = 'SELECT id, naam, aantal FROM selectiemaker_spelers';
-////$result2 = $db->query($sql2);
-////
-////array_push($json1,mysqli_fetch_all($result2, MYSQLI_ASSOC));
-//echo "Hier kom ik";
-//echo json_encode($json1);
-//$db->close();
-//?>
+?>
