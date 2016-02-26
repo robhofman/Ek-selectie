1ee41e659e88bf8b3435ed6b5b9650037e0fe1c1
Thibault V <thibault.vanluchene@student.howest.be>
2016-02-26 15:14:05 +0100

    reset database via admin console

--- admin/resetdatabase.php
+++ admin/resetdatabase.php
@@ -10,12 +10,12 @@ include 'dbconnect.php';
 
 $nul = 0;
 
-$sql = "UPDATE 'selectiemaker_inzendingen' SET totaal=$nul";
-if ($conn->query($sql) === TRUE) { echo "Totaal aantal inzendingen gereset \n"; } else { echo "Error updating record: " . $conn->error; }
+$sql = "UPDATE 'selectiemaker_inzendingen' SET totaal=0";
+if ($db->query($sql) === TRUE) { echo "Totaal aantal inzendingen gereset \n"; } else { echo "Error updating record: " . $db->error; }
 
 
 $sql2 = "UPDATE 'selectiemaker_spelers' SET aantal=0";
-if ($conn->query($sql2) === TRUE) { echo "Alle spelers gereset \n"; } else { echo "Error updating record: " . $conn->error; }
+if ($db->query($sql2) === TRUE) { echo "Alle spelers gereset \n"; } else { echo "Error updating record: " . $db->error; }
 
 
 
