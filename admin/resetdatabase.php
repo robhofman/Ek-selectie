5d56fa6c2476a6f69efa0248d605ed70d67120a8
Thibault V <thibault.vanluchene@student.howest.be>
2016-02-26 15:15:05 +0100

    SQL syntax error

--- admin/resetdatabase.php
+++ admin/resetdatabase.php
@@ -10,11 +10,11 @@ include 'dbconnect.php';
 
 $nul = 0;
 
-$sql = "UPDATE 'selectiemaker_inzendingen' SET totaal=0";
+$sql = "UPDATE selectiemaker_inzendingen SET totaal=0";
 if ($db->query($sql) === TRUE) { echo "Totaal aantal inzendingen gereset \n"; } else { echo "Error updating record: " . $db->error; }
 
 
-$sql2 = "UPDATE 'selectiemaker_spelers' SET aantal=0";
+$sql2 = "UPDATE selectiemaker_spelers SET aantal=0";
 if ($db->query($sql2) === TRUE) { echo "Alle spelers gereset \n"; } else { echo "Error updating record: " . $db->error; }
 
 
